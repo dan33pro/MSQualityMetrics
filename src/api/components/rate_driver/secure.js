de6e7f8688dc3a5b0dd27f932acb042863dba1f7
DagoGres <dan333pro@gmai.com>
2023-10-12 21:19:37 -0500

    Error en secure

--- src/api/components/rate_driver/secure.js
+++ src/api/components/rate_driver/secure.js
@@ -4,7 +4,7 @@ module.exports = function checkAuth(action) {
   function middlewware(req, res, next) {
     switch (action) {
       case "admin":
-        let owner = req.body.cc_administrador;
+        let owner = req.body.cc_ciudadano;
         auth.check.checkRol(req, owner);
         next();
         break;
